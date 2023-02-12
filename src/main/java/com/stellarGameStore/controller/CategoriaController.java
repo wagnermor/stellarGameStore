843352caff9ffa4434f9e2b21b92421b1e36bb4c
WagnerMor <wagnermor@outlook.com>
2023-02-12 00:50:17 -0300

    Fix: add post, put and delete methods in CategoriaController.java

--- src/main/java/com/stellarGameStore/controller/CategoriaController.java
+++ src/main/java/com/stellarGameStore/controller/CategoriaController.java
@@ -1,15 +1,24 @@
 package com.stellarGameStore.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.stellarGameStore.model.Categoria;
 import com.stellarGameStore.repository.CategoriaRepository;
@@ -34,10 +43,32 @@ public class CategoriaController {
 	}
 	
 	@GetMapping("/nome/{nome}")
-	public ResponseEntity<Categoria> getByNome(@PathVariable String nome) {
-		return repository.findByNome(nome)
-				.map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResposeEntity.status(HttpStatus.NOT_FOUND).build());
+	public ResponseEntity<List<Categoria>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria) {
+		return repository.findById(categoria.getId())
+				.map(resp -> ResponseEntity.status(HttpStatus.OK)
+						.body(repository.save(categoria)))
+						.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Categoria> categoria = repository.findById(id);
+		if(categoria.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		repository.deleteById(id);
+	}
+
 }
