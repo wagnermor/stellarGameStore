7b32ac6d3a6aa6e8ebf566f60fa9c6e36675fe4c
WagnerMor <wagnermor@outlook.com>
2023-02-11 23:30:12 -0300

    Add: /model/Produto.java class

---
+++ src/main/java/com/stellarGameStore/model/Produto.java
@@ -0,0 +1,78 @@
+package com.stellarGameStore.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter nomínimo 5 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "O atributo preço é obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo preço deve conter nomínimo 5 e no máximo 100 caracteres")
+	private BigDecimal preco;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório")
+	@Size(min = 10, max = 100, message = "O atributo descrição deve conter nomínimo 10 e no máximo 300 caracteres")
+	private String descricao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
