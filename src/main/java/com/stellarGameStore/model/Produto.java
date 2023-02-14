60f0e1f651e655f28fa2ce78b291f9ead8e725b1
WagnerMor <wagnermor@outlook.com>
2023-02-14 17:22:35 -0300

    Add: @ManyToOne relation with usuario class

--- src/main/java/com/stellarGameStore/model/Produto.java
+++ src/main/java/com/stellarGameStore/model/Produto.java
@@ -35,6 +35,10 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
